7fb9dfa922b04102a9ce5e1c2fc9d339c7b0beb2
MuhammadAslam-K <aslamka.2k3@gmail.com>
2024-11-16 18:24:38 +0530

    created the versel.json file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,21 +4,21 @@ import { getStorage } from 'firebase/storage';
 // import { setLogLevel } from "firebase/app";
 
 const firebaseConfig = {
-    // apiKey: "AIzaSyAhGmbcf1GcSfPeIjZndILWCC11sbsGTL4",
-    // authDomain: "pearl-test-d9450.firebaseapp.com",
-    // projectId: "pearl-test-d9450",
-    // storageBucket: "pearl-test-d9450.appspot.com",
-    // messagingSenderId: "223833263765",
-    // appId: "1:223833263765:web:656ac1d10eecaeea41959c",
-    // measurementId: "G-M034EWNHWH"
+    apiKey: "AIzaSyAhGmbcf1GcSfPeIjZndILWCC11sbsGTL4",
+    authDomain: "pearl-test-d9450.firebaseapp.com",
+    projectId: "pearl-test-d9450",
+    storageBucket: "pearl-test-d9450.appspot.com",
+    messagingSenderId: "223833263765",
+    appId: "1:223833263765:web:656ac1d10eecaeea41959c",
+    measurementId: "G-M034EWNHWH"
 
-    apiKey: "AIzaSyCZ0XDUXhXCvMaU5E1sZ3SY9c6RgD0TZg8",
-    authDomain: "coolpearl-a5f47.firebaseapp.com",
-    projectId: "coolpearl-a5f47",
-    storageBucket: "coolpearl-a5f47.firebasestorage.app",
-    messagingSenderId: "884171445309",
-    appId: "1:884171445309:web:04ddb273bfdf4b2955c654",
-    measurementId: "G-KV4MRL2HK6"
+    // apiKey: "AIzaSyCZ0XDUXhXCvMaU5E1sZ3SY9c6RgD0TZg8",
+    // authDomain: "coolpearl-a5f47.firebaseapp.com",
+    // projectId: "coolpearl-a5f47",
+    // storageBucket: "coolpearl-a5f47.firebasestorage.app",
+    // messagingSenderId: "884171445309",
+    // appId: "1:884171445309:web:04ddb273bfdf4b2955c654",
+    // measurementId: "G-KV4MRL2HK6"
 };
 
 // Initialize Firebase
